63078062d0bbe6610d06cfae4ed9fc9d59ada357
Skyler <skyler.m.degrote@outlook.com>
2016-08-12 21:16:28 -0500

    update config file

--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 
 
 $databasehost='localhost';
-$databaseuser='write-it';
-$databasepassword='writeitapp';
+$databaseuser='root';
+$databasepassword='root';
 $db=new mysqli($databasehost,$databaseuser,$databasepassword);
 if($db->connect_errno){
     die('Could not connect database');
